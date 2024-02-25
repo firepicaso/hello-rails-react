b77e04c024c3053ccf97318fe373265fb940f071
firepicaso <m.m.kiron@gmail.com>
2024-02-25 16:29:26 +0600

    Create app store

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,12 +3,19 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import App from "./App";
+import store from "./redux/store";
 
-function App() {
-  return (<h1>Hello World and the new world</h1>);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
